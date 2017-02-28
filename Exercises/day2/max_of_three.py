5b4706ae76c0e059d5dcd27e3fa0dc99946eae55
Daniel <danielems3@outlook.com>
2017-02-28 14:58:33 -0500

    Finalize function

--- Exercises/day2/max_of_three.py
+++ Exercises/day2/max_of_three.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def max_of_three(one,two,three):
-    pass
+    if(one > two and one >three):
+        return one
+    elif(two > one and two > three):
+        return two 
+    else:
+        return three
